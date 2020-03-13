e6f3183647e82118b49c2bd15f26376101c21174
Luis Antonio Rodriguez Garcia <luis@luisrodriguezgarcia.com>
2020-03-12 19:33:31 -0600

    Controladores: Definiendo la lógica de negocio

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = require('./network/routes');
+
+let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+//app.use(router);
+
+router(app);
+
+app.use('/app', express.static('public'));
+app.listen(3000);
+console.log('La aplicación está escuchando en http://localhost:3000');
+
